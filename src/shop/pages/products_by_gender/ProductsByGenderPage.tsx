a266e878356f3d9c04b046854748c3a1fa40f6a6
David Puentes <pipe01112009@gmail.com>
2026-04-25 19:49:37 -0500

    Designing ProductsByGenderPage.

--- src/shop/pages/products_by_gender/ProductsByGenderPage.tsx
+++ src/shop/pages/products_by_gender/ProductsByGenderPage.tsx
@@ -1,5 +1,36 @@
+import { useParams } from "react-router";
+
+import { productsMock } from "@/mocks/products.mock";
+
+import usePageNumberQuery from "@/shop/hooks/usePageNumberQuery";
+
+import AppPagination from "@/components/shared/AppPagination";
+import ProductsGrid from "@/shop/components/ProductsGrid";
+import ShopJumbotron from "@/shop/components/ShopJumbotron";
+
 const ProductsByGenderPage = function () {
-  return <div>ProductsByGenderPage</div>;
+  const [currentPage, setCurrentPage] = usePageNumberQuery();
+  const { gender } = useParams();
+
+  const genderLabels = {
+    men: "hombres",
+    women: "mujeres",
+    kids: "niños",
+  };
+
+  const handleUpdatePage = setCurrentPage;
+
+  return (
+    <>
+      <ShopJumbotron title={`Productos para ${genderLabels[gender]}`} />
+      <ProductsGrid products={productsMock} />
+      <AppPagination
+        totalPages={5}
+        currentPage={currentPage}
+        onUpdatePage={handleUpdatePage}
+      />
+    </>
+  );
 };
 
 export default ProductsByGenderPage;
