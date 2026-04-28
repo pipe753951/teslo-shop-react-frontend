b03ff3f4317c2f810fe5deb8d7c8b265161a0d0d
David Puentes <pipe01112009@gmail.com>
2026-04-28 15:28:18 -0500

    Removing undefined gender, because a redirection is planned to be done.

--- src/shop/pages/products_by_gender/ProductsByGenderPage.tsx
+++ src/shop/pages/products_by_gender/ProductsByGenderPage.tsx
@@ -14,7 +14,6 @@ const ProductsByGenderPage = function () {
   const { gender = "undefined" } = useParams();
 
   const genderLabels: Record<string, string> = {
-    undefined: "[género]",
     men: "hombres",
     women: "mujeres",
     kids: "niños",
