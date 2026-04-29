a198baaa9f493f5b0b15972445dfa1a09433743e
David Puentes <pipe01112009@gmail.com>
2026-04-29 16:43:05 -0500

    Finishing lesson #298.

---
+++ src/auth/actions/check-auth.action.ts
@@ -0,0 +1,19 @@
+import { tesloApi } from "@/api/tesloApi";
+import type { AuthResponse } from "@/types/interfaces/responses/auth.response";
+
+const checkAuthAction = async function (): Promise<AuthResponse> {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("No token found.");
+
+  try {
+    const { data } = await tesloApi.get<AuthResponse>("/auth/ckeck-status");
+
+    return data;
+  } catch {
+    throw new Error(
+      "Token expired or not valid, or something unexpected happened.",
+    );
+  }
+};
+
+export default checkAuthAction;
