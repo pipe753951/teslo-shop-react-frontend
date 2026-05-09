6a472edd87a834329a5dfd7cd61f2c9ecaac994b
David Puentes <pipe01112009@gmail.com>
2026-05-09 18:49:56 -0500

    Finishing lesson #309, making getProductByIdOrSlug function and modifying Product interface to make user property optional.

---
+++ src/shop/actions/get-product-by-id-or-slug.ts
@@ -0,0 +1,41 @@
+import { TESLO_API_BASE_URL, tesloApi } from "@/api/tesloApi";
+
+import type { Product } from "@/types/interfaces/product.interface";
+
+const getProductByIdOrSlug = async function (
+  idOrSlug: string,
+  allowNewProduct?: boolean,
+): Promise<Product> {
+  if (!idOrSlug) {
+    throw new Error(
+      "ID or Slug isn't specified. Please specify it when you want to call this function.",
+    );
+  }
+
+  if (idOrSlug === "new" && allowNewProduct) {
+    return {
+      id: "new",
+      title: "",
+      price: 0,
+      description: "",
+      slug: "",
+      stock: 1,
+      sizes: [],
+      gender: "women",
+      tags: [],
+      images: [],
+    };
+  }
+
+  const { data: responseData } = await tesloApi.get<Product>(
+    `/products/${idOrSlug}`,
+  );
+
+  const mappedImages = responseData.images.map(
+    (image) => `${TESLO_API_BASE_URL}/files/product/${image}`,
+  );
+
+  return { ...responseData, images: [...mappedImages] };
+};
+
+export default getProductByIdOrSlug;
